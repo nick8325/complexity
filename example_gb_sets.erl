eab2883ae7fc40e822eb0af4edd266e194289cf5
Nick Smallbone <nick.smallbone@gmail.com>
2013-08-30 11:25:21 +0200

    trying insertion and deletion in gb_sets

--- example_gb_sets.erl
+++ example_gb_sets.erl
@@ -17,3 +17,40 @@ gb_sets_gen({X, T}) ->
        [ {X, gb_sets:add(Y, T)},
          {Y, T},
          {Y, gb_sets:add(Y, T)} ])).
+
+cmds(T) ->
+  [{insert, resize(100, int())} ] ++
+  [{delete, elements(gb_sets:to_list(T))} || gb_sets:size(T) > 0].
+
+eval_cmd({insert, X}, T) ->
+  gb_sets:add(X, T);
+eval_cmd({delete, X}, T) ->
+  gb_sets:delete_any(X, T).
+
+eval_cmds([], T) ->
+  T;
+eval_cmds([X|Xs], T) ->
+  eval_cmd(X, eval_cmds(Xs, T)).
+
+construct([Cmd|Cmds]) ->
+  {Cmd, Cmds, eval_cmds(Cmds, gb_sets:empty())}.
+
+gen_tree({Cmd, Cmds, T}) ->
+  T1 = eval_cmd(Cmd, T),
+  ?LET(Cmds1, cmds(T1),
+  return(
+  [ construct(Cmds2)
+  || Cmd1 <- Cmds1,
+     Cmds2 <- example_sorting:insert_anywhere(Cmd1, [Cmd|Cmds]) ])).
+
+is_insert({insert, _}) ->
+  true;
+is_insert(_) ->
+  false.
+
+measure_gb_sets() ->
+    measure(5, 100,
+            fun({_, Xs, _}) -> length(Xs) end,
+            construct([{insert,0}]),
+            fun gen_tree/1,
+            fun({X, Xs, _T}) -> construct([X|Xs]) end).
