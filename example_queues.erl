90190444bc4a99186ae031c69738277917a6708c
Nick Smallbone <nick.smallbone@gmail.com>
2014-05-08 08:05:57 +0200

    Update queue example.

--- example_queues.erl
+++ example_queues.erl
@@ -5,75 +5,33 @@
 -include("measure.hrl").
 -include_lib("eqc/include/eqc.hrl").
 
-%% Argh! lists:reverse makes a constant number of reductions.
-reverse(Xs) ->
-    reverse(Xs, []).
-reverse([], Ys) ->
-    Ys;
-reverse([X|Xs], Ys) ->
-    reverse(Xs, [X|Ys]).
-
-in1(X, {Xs, Ys}) ->
-    {[X|Xs], Ys}.
-in2(Y, {Xs, Ys}) ->
-    {Xs, [Y|Ys]}.
-out1({[], []}) ->
-    error(empty_queue);
-out1({[], Ys}) ->
-    [X|Xs] = reverse(Ys),
-    {X, {Xs, []}};
-out1({[X|Xs], Ys}) ->
-    {X, {Xs, Ys}}.
-out2({[], []}) ->
-    error(empty_queue);
-out2({Xs, []}) ->
-    [Y|Ys] = reverse(Xs),
-    {Y, {[], Ys}};
-out2({Xs, [Y|Ys]}) ->
-    {Y, {Xs, Ys}}.
-len({Xs, Ys}) ->
-    length(Xs) + length(Ys).
-empty({[], []}) ->
-    true;
-empty(_) ->
-    false.
-new() ->
-    {[], []}.
-
 %% Generate and interpret "commands".
-cmd({out,1}, Q) ->
-    element(2, out1(Q));
-cmd({out,2}, Q) ->
-    element(2, out2(Q));
-cmd({in,1}, Q) ->
-    in1(a, Q);
-cmd({in,2}, Q) ->
-    in2(a, Q).
+cmd(out, Q) -> element(2, queues:out(Q));
+cmd(out_r, Q) -> element(2, queues:out_r(Q));
+cmd({in, X}, Q) -> queues:in(X, Q);
+cmd({in_r, X}, Q) -> queues:in_r(X, Q).
 
-cmds(Cmds) ->
-    cmds(Cmds, {[],[]}).
-cmds([], Q) ->
-    Q;
-cmds([Cmd|Cmds], Q) ->
-    cmds(Cmds, cmd(Cmd, Q)).
+cmds(Cmds) -> cmds(Cmds, queues:new()).
 
-valid_cmds(Cmds) ->
-    valid_cmds(0, Cmds).
-valid_cmds(_, []) ->
-    true;
-valid_cmds(0, [{out,_}|_]) ->
-    false;
-valid_cmds(N, [{out,_}|Cmds]) ->
-    valid_cmds(N-1, Cmds);
-valid_cmds(N, [{in,_}|Cmds]) ->
-    valid_cmds(N+1, Cmds).
+cmds([], Q) -> Q;
+cmds([Cmd|Cmds], Q) -> cmds(Cmds, cmd(Cmd, Q)).
+
+valid_cmds(Cmds) -> valid_cmds(0, Cmds).
+
+valid_cmds(_, []) -> true;
+valid_cmds(0, [out|_]) -> false;
+valid_cmds(0, [out_r|_]) -> false;
+valid_cmds(N, [out|Cmds]) -> valid_cmds(N-1, Cmds);
+valid_cmds(N, [out_r|Cmds]) -> valid_cmds(N-1, Cmds);
+valid_cmds(N, [{in,_}|Cmds]) -> valid_cmds(N+1, Cmds);
+valid_cmds(N, [{in_r,_}|Cmds]) -> valid_cmds(N+1, Cmds).
 
 gen_cmds(Cmds) ->
     Candidates =
-      example_sorting:insert_anywhere([{out,1}, {out,2}, {in,1}, {in,2}], Cmds),
+      example_sorting:insert_anywhere([out, out_r, {in,a}, {in_r,a}], Cmds),
     lists:filter(fun valid_cmds/1, Candidates).
 
-measure_queue() ->
+measure_queues() ->
     measure(1, 50,
             #family{initial=[], grow=fun gen_cmds/1},
             #axes{size=fun length/1, time=time1(fun cmds/1)}).
