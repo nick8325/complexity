093d1262f9b5e31167062b12f843a93fe7875199
Tobias Gedell <gedell@chalmers.se>
2015-03-20 13:09:17 +0100

    Added measure_java:with_java that allows you to run a function in a Java environment.

--- src/measure_java.erl
+++ src/measure_java.erl
@@ -30,20 +30,32 @@ run_java_commands(GC, Iterations, SetupCmdsString, CmdsString, TeardownCmdsStrin
   java:call_static(get_java_node(), 'Complexity', measure, [GC, Iterations, SetupCmdsString, CmdsString, TeardownCmdsString]) / 1000000.
 
 
+%% Runs the provided function in a Java environment.
+%% I.e., it will make sure that a Java node is started
+%% before running the function.
+with_java(ClassPaths, RunFun) ->
+  start_java_node(ClassPaths),
+  Result = RunFun(),
+  stop_java_node(),
+  Result.
+
+
 %% Measure functions.
 measure_java(Rounds, MaxSize, Family, Axes, ClassPaths) ->
   measure_java(Rounds, MaxSize, Family, Axes, ClassPaths, fun() -> ok end, fun() -> ok end).
 
 measure_java(Rounds, MaxSize, Family, Axes, ClassPaths, SetupFun, TeardownFun) ->
+  RunFun =
+    fun() ->
+      SetupFun(),
+      WarmUpGrow = fun(X) -> ?LET(Lst, (Family#family.grow)(X), [eqc_gen:oneof(Lst)]) end,
+      io:format("Warming up the Java VM...~n"),
+      measure(1, MaxSize, Family#family{ grow = WarmUpGrow, warmup = true }, Axes),
+      io:format("~nRunning the complexity benchmarks...~n"),
+      Result = measure(Rounds, MaxSize, Family, Axes),
+      TeardownFun(),
+      Result
+    end,
   eqc:start(),
-  start_java_node(ClassPaths),
-  SetupFun(),
-  WarmUpGrow = fun(X) -> ?LET(Lst, (Family#family.grow)(X), [eqc_gen:oneof(Lst)]) end,
-  io:format("Warming up the Java VM...~n"),
-  measure(1, MaxSize, Family#family{ grow = WarmUpGrow, warmup = true }, Axes),
-  io:format("~nRunning the complexity benchmarks...~n"),
-  Result = measure(Rounds, MaxSize, Family, Axes),
-  TeardownFun(),
-  stop_java_node(),
-  Result.
+  with_java(ClassPaths, RunFun).
 
